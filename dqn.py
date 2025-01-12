7e726c5c195f0e199011c16690987df0e8372a12
Luka-Radulovic <lukaradulovikj@gmail.com>
2025-01-12 18:35:14 +0100

    'DQN agent added'

---
+++ dqn.py
@@ -0,0 +1,109 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pearl.neural_networks.sequential_decision_making.q_value_networks import VanillaQValueNetwork
+from pearl.utils.functional_utils.experimentation.set_seed import set_seed
+from pearl.policy_learners.sequential_decision_making.deep_q_learning import DeepQLearning
+from pearl.policy_learners.sequential_decision_making.double_dqn import DoubleDQN
+from pearl.replay_buffers import BasicReplayBuffer
+from pearl.utils.functional_utils.train_and_eval.online_learning import online_learning
+from pearl.pearl_agent import PearlAgent
+from pearl.utils.instantiations.environments.gym_environment import GymEnvironment
+from pearl.action_representation_modules.one_hot_action_representation_module import (
+    OneHotActionTensorRepresentationModule,
+)
+
+set_seed(216028)
+
+env = GymEnvironment("Acrobot-v1")
+
+num_actions = env.action_space.n
+
+# VanillaQValueNetwork class uses a simple mlp for approximating the Q values.
+#  - Input dimension of the mlp = (state_dim + action_dim)
+#  - Size of the intermediate layers are specified as list of `hidden_dims`.
+hidden_dims = [128,64,32]
+
+
+# We will be using a one hot representation for representing actions. So take action_dim = num_actions.
+Q_value_network = VanillaQValueNetwork(state_dim=env.observation_space.shape[0],  # dimension of the state representation
+                                       action_dim=num_actions,                    # dimension of the action representation
+                                       hidden_dims=hidden_dims,                   # dimensions of the intermediate layers
+                                       output_dim=1)       
+
+
+
+
+# Instead of using the 'network_type' argument, use the 'network_instance' argument.
+# Pass Q_value_network as the `network_instance` to the `DeepQLearning` policy learner.
+DQNagent = PearlAgent(
+    policy_learner=DeepQLearning(
+        state_dim=env.observation_space.shape[0],
+        action_space=env.action_space,
+        batch_size=64,
+        training_rounds=10,
+        soft_update_tau=0.75,
+        network_instance=Q_value_network, # pass an instance of Q value network to the policy learner.
+        action_representation_module=OneHotActionTensorRepresentationModule(
+            max_number_actions=num_actions
+        ),
+    ),
+    replay_buffer=BasicReplayBuffer(10_000),
+    )
+
+
+
+info = online_learning(
+    agent=DQNagent,
+    env=env,
+    number_of_episodes=500,
+    print_every_x_episodes=20,   # print returns after every 10 episdoes
+    learn_after_episode=True,    # instead of updating after every environment interaction, Q networks are updates at the end of each episode
+    seed=0
+
+
+)
+
+
+# env = GymEnvironment('Acrobot-v1', render_mode='human')
+
+# observation, action_space = env.reset()
+# DQNagent.reset(observation, action_space)
+
+
+NUM_EPISODES = 10 
+cumulative_reward = 0
+cumulative_return = 0 
+
+for i in range(NUM_EPISODES):
+
+    env = GymEnvironment('Acrobot-v1', render_mode='human')
+    observation, action_space = env.reset()
+    DQNagent.reset(observation, action_space)
+    done = False
+
+    while not done:
+
+        action = DQNagent.act(exploit=True)  
+        action_result = env.step(action)
+        env.render() 
+        observation = action_result.observation
+        reward = action_result.reward
+        done = action_result.done
+        DQNagent.observe(action_result)
+        cumulative_reward += reward
+
+    cumulative_return += cumulative_reward
+
+
+print(f"Average return for DQN: {cumulative_return/NUM_EPISODES}")
+env.close()
+
+torch.save(info["return"], "DQN-return.pt")   # info["return"] refers to the episodic returns
+plt.plot(np.arange(len(info["return"])), info["return"], label="DQN")
+plt.title("Episodic returns")
+plt.xlabel("Episode")
+plt.ylabel("Return")
+plt.legend()
+plt.show()
